ae9f85b7617ea7d452ad3e90b6111071ff352f8d
Alexandru <lexyus22@gmail.com>
2026-05-03 10:21:14 +0200

    Add format_data function.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from random import choice
 from data import questions
 
+def format_data(question):
+    data_q = question['question']
+    data_a = question['a']
+    data_b = question['b']
+
+    return f'{data_q}\n Answer is: {data_a} or {data_b} ?'
+
