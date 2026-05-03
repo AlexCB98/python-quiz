a3d5933c06c871e8f93f3102fb60378524f31c9c
Alexandru <lexyus22@gmail.com>
2026-05-03 11:07:59 +0200

    Add main game loop with lives and score system.

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from random import choice
 from data import questions
 
-def format_data(question):
-    data_q = question['question']
-    data_a = question['a']
-    data_b = question['b']
+def format_data(questions):
+    data_q = questions['question']
+    data_a = questions['a']
+    data_b = questions['b']
 
-    return f'{data_q}\n Answer is: {data_a} or {data_b} ?'
+    return f'{data_q}\na) {data_a}\nb) {data_b}'
 
+def check_answer(answer, c_answer):
+    return answer == c_answer
+
+score = 0
+lives = 5
+
+
+
+for question in questions:
+    print(format_data(question))
+    guess = input('Type you answer "A" or "B" ?: ').lower()
+
+    if check_answer(guess, question['answer']):
+        score +=1
+        print(f'\nCorrect. Score: {score}\n')
+    else:
+        lives -=1
+        print(f'\nWrong. Lives left: {lives}\n')
+
+    if lives == 0:
+        print('You are out of lives.')
+        break
+
+print(f'Final score: {score}/{len(questions)}')
